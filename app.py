7680e7ab930f034db37c5e21c30bc4225640f3de
Formshost <90462460+Formshost@users.noreply.github.com>
2024-06-05 23:38:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,29 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# File to store the submission count
+count_file = 'submission_count.txt'
+
+# Function to read the count from the file
+def read_count():
+    if os.path.exists(count_file):
+        with open(count_file, 'r') as file:
+            try:
+                count = int(file.read())
+            except ValueError:
+                count = 0
+    else:
+        count = 0
+    return count
+
+
+# Function to write the count to the file
+def write_count(count):
+    with open(count_file, 'w') as file:
+        file.write(str(count))
+
+# Read the current submission count
+submission_count = read_count()
 
 
 # Define the Streamlit form for user input
@@ -145,6 +168,9 @@ if submit_button:
         for error in errors:
             st.error(error)
     else:
+        # Increment the submission counter
+        submission_count += 1
+        write_count(submission_count)
         # Create a DataFrame to hold the user input data
         input_data = pd.DataFrame({
             'Age': [age], 'BusinessTravel': [business_travel],
