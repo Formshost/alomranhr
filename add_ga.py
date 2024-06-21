b4eb86c45c27a3c12f24a19df5f63ae22caadfe2
Formshost <90462460+Formshost@users.noreply.github.com>
2024-06-21 23:49:58 +0300

    Update add_ga.py

--- add_ga.py
+++ add_ga.py
@@ -17,18 +17,29 @@ GA_SCRIPT = """
 
 def inject_ga():
     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    print(f"Index path: {index_path}")
+    
     if not index_path.exists():
         print("index.html does not exist")
         return
+    
+    print("index.html found")
+    
     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
     if not soup.find(id=GA_ID): 
+        print("GA script not found, injecting...")
+        
         bck_index = index_path.with_suffix('.bck')
         if bck_index.exists():
             shutil.copy(bck_index, index_path)  
         else:
             shutil.copy(index_path, bck_index)  
+        
         html = str(soup)
         new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
         index_path.write_text(new_html)
+        print("GA script injected successfully")
+    else:
+        print("GA script already exists")
 
 inject_ga()
