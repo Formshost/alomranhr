ace0a5f58f6f7b9b1b0c6dd5ce6b5a95e016a141
Formshost <90462460+Formshost@users.noreply.github.com>
2024-06-21 20:46:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,30 +26,19 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Add Google Analytics tracking code
-def inject_ga():
-    GA_ID = "GT-NCLQ5ZJ8"  # Replace with your actual Tracking ID
-    GA_JS = f"""
-    <script async src="https://www.googletagmanager.com/gtag/js?id={GA_ID}"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){{dataLayer.push(arguments);}}
-      gtag('js', new Date());
-      gtag('config', '{GA_ID}');
-    </script>
-    """
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)
-        else:
-            shutil.copy(index_path, bck_index)
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
+ga_tracking_code = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=GT-NCLQ5ZJ8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'GT-NCLQ5ZJ8');
+</script>
+"""
 
-inject_ga()
+# Inject Google Analytics script into the app
+components.html(ga_tracking_code, height=0)
 
 
 
