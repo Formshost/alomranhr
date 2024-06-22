ea473ac7f95279c9adac1429b11b4d64a2ddd771
Formshost <90462460+Formshost@users.noreply.github.com>
2024-06-22 04:56:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,19 @@ hide_streamlit_style = """
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+import streamlit as st
+from streamlit_analytics import StreamlitAnalytics
+
+# Initialize Streamlit Analytics with your tracking ID
+StreamlitAnalytics(
+    tracking_id="G-WZGPN73NKB",
+).start()
+
+# Your Streamlit app code goes here
+st.title("My Streamlit App")
+st.write("Hello, World!")
+
+
 # Define the model version
 model_version = '1.0'  # You can change this as needed
 
