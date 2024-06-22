187859bda2236131d994b33df65ac2575d5895b4
Formshost <90462460+Formshost@users.noreply.github.com>
2024-06-22 18:48:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 import os
 from model import predict, predict_proba  # Ensure these functions handle DataFrame input
-
+import streamlit.components.v1 as components
 
 
 
@@ -24,6 +24,26 @@ hide_streamlit_style = """
     </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+# Google Analytics script
+GA_SCRIPT = """
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WZGPN73NKB"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-WZGPN73NKB');
+</script>
+"""
+
+# Inject the Google Analytics script into the Streamlit app
+components.html(GA_SCRIPT, height=0, width=0)
+
+# Rest of your Streamlit app code
+st.title('My Streamlit App')
+# Add more Streamlit components as needed
 
 
 # Define the model version
