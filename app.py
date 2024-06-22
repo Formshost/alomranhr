c7008c03af97080cae44d8e0d4fb74e5db6627c0
Formshost <90462460+Formshost@users.noreply.github.com>
2024-06-22 20:13:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,13 +28,14 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Google Analytics script
 GA_SCRIPT = """
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GT-WB74Z346');</script>
-<!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CMZYYS73E2"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-CMZYYS73E2');
+</script>
 """
 
 # Inject the Google Analytics script into the Streamlit app
