17cbe35a1c630d22fde8146a8d3b7657859f54ab
Formshost <90462460+Formshost@users.noreply.github.com>
2024-06-23 22:10:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -139,13 +139,12 @@ with st.form("attrition_form"):
 
     st.markdown("Years With Current Manager / عدد السنوات مع المدير الحالي")
     years_with_curr_manager = st.number_input("", min_value=0, key='years_with_curr_manager', help="Enter the number of years with the current manager.")
-
-def track_prediction_click():
-    components.html("trackPredictionClick()")
+    
     submit_button = st.form_submit_button("Predict Attrition")
         track_prediction_click()
 
-
+def track_prediction_click():
+    components.html("trackPredictionClick()")
 # Handling form submission
 if submit_button:
     errors = []
